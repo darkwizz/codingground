ad014b2add54b8939efacef1356e5736e25d74b8
darkwizz <darkwizz@users.noreply.github.com>
2015-12-07 14:23:46 +0200

    Update polynomDivider.py

--- New Project/polynomDivider.py
+++ New Project/polynomDivider.py
@@ -6,7 +6,7 @@ def divide(left, right):
     for i in range(len(left)):
         xorRes = left[i] ^ right[i]
         if not isFirstOne and xorRes == 0:
-            print 'No any first 1'
+            #print 'No any first 1'
             continue
         elif not isFirstOne and xorRes == 1:
             isFirstOne = True
@@ -18,19 +18,26 @@ def getRest(inputBits, polynom):
     index = 0
     rest = []
     while index < len(inputBits):
-        print 'Index =', index
+        #print 'Index =', index
         rightBoard = index + pollen
-        print 'Right board =', rightBoard
+        #print 'Right board =', rightBoard
         if rightBoard > len(inputBits):
-            print 'In if part'
+            #print 'In if part'
             break
         rest.extend(inputBits[index + len(rest) : rightBoard])
-        print 'After extending:', rest
+        #print 'After extending:', rest
         rest = divide(rest, polynom)
-        print 'The rest =', rest
+        #print 'The rest =', rest
         index = index + pollen - len(rest)
-    print 'In end of getRest'
+    #print 'In end of getRest'
     return rest
+
+def isNormal(rest):
+    onesCount = 0
+    for i in rest:
+        if i:
+            onesCount += 1
+    return onesCount <= 2
 
 inputBits = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1]
 polynom = [1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1]
@@ -38,4 +45,12 @@ polynom = [1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1]
 print '\n\n'
 print getRest(inputBits, polynom)
 
-offsetNum = 0
+rest = getRest(inputBits, polynom)
+leftBoard = 0
+while not isNormal(rest):
+    leftBoard += 1
+    temp = inputBits[leftBoard : len(inputBits)]
+    temp.extend(inputBits[0 : leftBoard])
+    rest = getRest(temp, polynom)
+print 'Needed offset:', leftBoard
+print rest
