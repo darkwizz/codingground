e6229bef792216cc331410323aa9535384129ba2
darkwizz <darkwizz@users.noreply.github.com>
2015-12-11 08:52:50 +0200

    Update B4H.py

--- New Project/B4H.py
+++ New Project/B4H.py
@@ -3,18 +3,10 @@ import math
 
 def isPrime(number):
     end = int(number ** 0.5)
-    for i in range(end):
+    for i in range(2, end + 1):
         if number % i == 0:
             return True
     return False
-
-def getPrime(lastPrime = 2):
-    if lastPrime < 2:
-        return 2
-    for i in range(lastPrime ** 2):
-        if isPrime(lastPrime):
-            return i
-    return None
 
 def multiplyPolynoms(left, right):
     bRight = bin(right)[2:]
@@ -37,11 +29,15 @@ def getCreatingPolynom(*polynoms):
         resPolynom = multiplyPolynoms(resPolynom, polynoms[i])
     return resPolynom
 
-def getCreatingPolynom(fullCodeLength, mistakesCount):
-    h = math.log(fullCodeLength + 1.0, 2)
-    if h > int(h):
-        h = int(h) + 1
-    codeLength = 2 * mistakesCount + 1
-    polynomDegree = 2 * mistakesCount - 1
-    polynom = 0
-    
+def getMinimalPolynoms(fullCodeLength, mistakesCount):
+    polynoms = []
+    lastPolynom = 2 ** (mistakesCount * 2 - 1)
+    for i in range(mistakesCount):
+        polynom = 0
+        for j in range(lastPolynom, 0, -1):
+            if isPrime(j):
+                polynom = j
+                lastPolynom = polynom
+                polynoms.append(bin(polynom)[2:])
+                break
+    return polynoms
