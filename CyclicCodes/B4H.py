1cebc0ee5bfba07e1c9790da93bcc09141a93ff0
darkwizz <psyhoge@gmail.com>
2015-12-16 13:59:13 +0200

    Make work version of BCH coder

--- CyclicCodes/B4H.py
+++ CyclicCodes/B4H.py
@@ -1,6 +1,17 @@
 import polynomDivider as div
 import math
 
+#isn't used now
+_simplePolynomsTable = {
+    2: { 1: '111' },
+    3: { 1: '1011', 3: '1101' },
+    4: { 1: '10011', 3: '11111', 5: '111', 7: '11001' },
+    5: { 1: '100101', 3: '111101', 5: '110111', 7: '101111', 9: '110111', 11: '111011' },
+    6: { 1: '1000011', 3: '1010111', 5: '1100111', 7: '1001001', 9: '1101', 11: '1101101' },
+    7: { 1: '10001001', 3: '10001111', 5: '10011101', 7: '11110111', 9: '10111111', 11: '11010101', 13: '10000011' },
+    8: { 1: '100011101', 3: '101110111', 5: '111110011', 7: '101101001', 9: '110111101', 11: '111100011', 13: '100101011' }
+}
+
 def isPrime(number):
     i = 2
     while i * i < number:
@@ -8,6 +19,10 @@ def isPrime(number):
             return False
         i += 1
     return True
+
+def isAllowed(length):
+    sLength = bin(length + 1)[2:]
+    return sLength.count('1') == 1 and length != 1
 
 def multiplyPolynoms(left, right):
     bRight = bin(right)[2:]
@@ -23,32 +38,32 @@ def multiplyPolynoms(left, right):
         res ^= part
     return res
 
-def getCreatingPolynom(*polynoms):
+def getCreatingPolynom(polynoms):
     count = len(polynoms)
     resPolynom = polynoms[0]
     for i in range(1, count):
-        resPolynom = multiplyPolynoms(resPolynom, polynoms[i])
+        iResPolynom = int(resPolynom, 2)
+        iRight = int(polynoms[i], 2)
+        resPolynom = multiplyPolynoms(iResPolynom, iRight)
     return resPolynom
 
 def getMinimalPolynoms(fullCodeLength, mistakesCount):
     polynoms = []
-    highestDegree = math.log(fullCodeLength + 1.0, 2)
-    if highestDegree > int(highestDegree):
-        highestDegree = int(highestDegree) + 1
-    lastPolynom = 2 ** highestDegree
+    highestDegree = math.log(fullCodeLength + 1, 2)
     for i in range(mistakesCount):
-        polynom = 0
+        lastPolynom = 2 << int(highestDegree)
+        lastPolynom -= 1
+        highestDegree -= 1
         for j in range(lastPolynom, 0, -1):
             if isPrime(j):
                 polynom = j
-                lastPolynom = polynom
                 polynoms.append(bin(polynom)[2:])
                 break
     return polynoms
 
 def getBitsList(polynom):
     bits = bin(polynom)[2:]
-    res = [x for x in bits]
+    res = [int(x) for x in bits]
     return res
 
 def addBits(left, right):
@@ -58,7 +73,7 @@ def addBits(left, right):
         right = temp
     res = left[:]
     for i in range(-1, -len(right), -1):
-        res[i] = (int)res[i] ^ (int)right[i]
+        res[i] = int(res[i]) ^ int(right[i])
     return res
 
 def getB4HCode(inputBits, fullCodeLength, mistakesCount):
@@ -69,11 +84,12 @@ def getB4HCode(inputBits, fullCodeLength, mistakesCount):
         inputBits += ('0' * controlBitsCount)
     else:
         inputBits.extend([0] * controlBitsCount)
-    rest = getRest(inputBits, getBitsList(creatingPolynom)
+    rest = div.getRest(inputBits, getBitsList(creatingPolynom))
     result = addBits(inputBits, rest)
     return result
 
 if __name__ == "__main__":
-    inputBits = []
+    inputBits = [1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,1,1,1,1,0,1]
     codeLength = 31
     mistakesCount = 2
+    print getB4HCode(inputBits, codeLength, mistakesCount)
